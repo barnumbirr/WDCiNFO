0ffe11fd2120b8e0b5c5c6c45739ca3193d9d404
c0ding <me@martinsimon.me>
2014-04-07 23:01:35 +0200

    remove MtGox from statistics

--- wdcinfo.py
+++ wdcinfo.py
@@ -14,25 +14,27 @@ def get_info():
 	""" Fetches price and network difficulty from wdcticker.com """
 	data = requests.get('http://wdcticker.com/api/ticker')
 	d = json.loads(data.text)
-	values = [d[u'cryptsy_set'],d[u'vircurex_set'],d[u'crypto_trade_set'],d[u'coinbase_btc_set'],d[u'mtgox_btc_set'],d[u'btc_e_btc_set']]
-	keys= [u'cryptsy_set',u'vircurex_set',u'crypto_trade_set',u'coinbase_btc_set',u'mtgox_btc_set',u'btc_e_btc_set']
+	values = [d[u'cryptsy_set'],d[u'vircurex_set'],d[u'crypto_trade_set'],d[u'coinbase_btc_set'],d[u'btc_e_btc_set']]
+	keys= [u'cryptsy_set',u'vircurex_set',u'crypto_trade_set',u'coinbase_btc_set',u'btc_e_btc_set']
 	for key,value in zip(keys,values):
 		if value == True:
 			d[key] = "\033[32m✔\033[39m"
 		else:
 			d[key] = "\033[31m✘\033[39m"
 
-	if d[u'health_rating']>= 6:
-		d[u'health_rating'] = "\033[32m" + str(d[u'health_rating']) + str("/6") + "\033[39m"
+	d[u'health_rating'] = d[u'health_rating'] - 1
+
+	#if d[u'health_rating']>= 6:
+		#d[u'health_rating'] = "\033[32m" + str(d[u'health_rating']) + str("/6") + "\033[39m"
 	
-	elif d[u'health_rating'] >= 5:
-		d[u'health_rating'] = "\033[32m" + str(d[u'health_rating']) + "/6" + "\033[39m"
+	if d[u'health_rating'] >= 5:
+		d[u'health_rating'] = "\033[32m" + str(d[u'health_rating']) + "/5" + "\033[39m"
 	
 	elif 4 >= d[u'health_rating'] >= 3:
-		d[u'health_rating'] = "\033[33m" + str(d[u'health_rating']) + "/6" + "\033[39m"
+		d[u'health_rating'] = "\033[33m" + str(d[u'health_rating']) + "/5" + "\033[39m"
 	
 	elif d[u'health_rating'] <= 2:
-		d[u'health_rating'] = "\033[31m" + str(d[u'health_rating']) + "/6" + "\033[39m"
+		d[u'health_rating'] = "\033[31m" + str(d[u'health_rating']) + "/5" + "\033[39m"
 	
 	d[u'network_diff'] = '%.3f' % d[u'network_diff']
 	
@@ -75,7 +77,7 @@ def output(d, hashrate, market_cap):
 	print "WDC/BTC average: " + d[u'wdc_btc_avg'] + " BTC\n"
 	print "\033[4mWorldcoin price health:\033[0m\n"
 	print "Cryptsy   :  " + d[u'cryptsy_set'] + "         Vircurex  :  " + d[u'vircurex_set'] + "         Crypto Trade  :  " + d[u'crypto_trade_set']
-	print "Coinbase  :  " + d[u'coinbase_btc_set'] + "         mtgox     :  " + d[u'coinbase_btc_set'] + "         BTC-E         :  " + d[u'btc_e_btc_set']
+	print "Coinbase  :  " + d[u'coinbase_btc_set'] +  "         BTC-E     :  " + d[u'btc_e_btc_set']
 	print "Overall Worldcoin price health: " + d[u'health_rating']
 	print "\n\033[4mGeneral Worldcoin Stats:\033[0m\n "
 	print "Network difficulty: " + str(d[u'network_diff']) + "             Market cap (last 24h): " + market_cap[0]
